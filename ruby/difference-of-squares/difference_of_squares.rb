83be7e6666eea4190c654719439afa1f9c36df13
Sam Neely <sneely@samneely.org>
2015-05-11 12:40:42 -0500

    Adding method for squaring nums.

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -5,16 +5,22 @@ class Squares
   end
 
   def square_of_sums
-    sums = @natural_numbers.inject(:+)
-    sums**2
+    sum = @natural_numbers.inject(:+)
+    square_number(sum)
   end
 
   def sum_of_squares
-    squares = @natural_numbers.map { |n| n**2 }
+    squares = @natural_numbers.map { |n| square_number(n) }
     squares.inject(:+)    
   end
   
   def difference
     square_of_sums - sum_of_squares
+  end
+
+  private
+
+  def square_number(number)
+    number**2
   end
 end
