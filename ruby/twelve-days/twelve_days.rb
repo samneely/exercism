24b7bdfc5d4d24a2388c0f72552a8a3fcebea72f
Sam Neely <sneely@samneely.org>
2017-12-12 09:08:56 -0500

    Extracting an Ordinality refinement for ordinal days
    Using a refinement instead of monkey-patching to encapsulate the
    behavior and require explicit inclusion so it doesn't leak into other
    places using Numeric

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,4 +1,31 @@
+module Ordinality
+  refine Numeric do
+    ORDINALS = {
+      1 => "first",
+      2 => "second",
+      3 => "third",
+      4 => "fourth",
+      5 => "fifth",
+      6 => "sixth",
+      7 => "seventh",
+      8 => "eighth",
+      9 => "ninth",
+      10 => "tenth",
+      11 => "eleventh",
+      12 => "twelfth"
+    }
+
+    def to_ordinal
+      ORDINALS.fetch(self) do
+        raise ArgumentError, "No ordinal defined for #{self}"
+      end
+    end
+  end
+end
+
 class TwelveDays
+  using Ordinality
+
   def self.song
     1.upto(12).reduce("") { |song, day| song += verse_for(day) }
   end
@@ -6,29 +33,29 @@ class TwelveDays
   def self.verse_for(day)
     case day
     when 12
-      "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n"
     when 11
-      "On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 10
-      "On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 9
-      "On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 8
-      "On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 7
-      "On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 6
-      "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 5
-      "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 4
-      "On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 3
-      "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     when 2
-      "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree." + "\n\n"
     else
-      "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree." + "\n\n"
+      "On the #{day.to_ordinal} day of Christmas my true love gave to me, a Partridge in a Pear Tree." + "\n\n"
     end
   end
 end
